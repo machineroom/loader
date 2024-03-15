f8f14154846848c691ba46ef264a492bd4537480
James <macihenroomfiddling@gmail.com>
2024-03-15 18:51:38 +0000

    update to RP5 rpi_c011 lib

---
+++ gpiochip_rp1.h
@@ -0,0 +1,14 @@
+void rp1_gpio_get_bank(int num, int *bank, int *offset);
+uint32_t rp1_gpio_sys_rio_out_read(volatile uint32_t *base, int bank, int offset);
+void rp1_gpio_sys_rio_out_write(volatile uint32_t *base, int bank, int offset, uint32_t value);
+uint32_t rp1_gpio_sys_rio_sync_in_read(volatile uint32_t *base, int bank,
+                                              int offset);
+
+
+void rp1_gpio_sys_rio_oe_clr(volatile uint32_t *base, int bank, int offset);
+void rp1_gpio_sys_rio_oe_set(volatile uint32_t *base, int bank, int offset);
+
+void rp1_gpio_sys_rio_oe_set_word(volatile uint32_t *base, int bank, uint32_t word);
+void rp1_gpio_sys_rio_oe_clr_word(volatile uint32_t *base, int bank, uint32_t word);
+
+uint32_t rp1_gpio_sys_rio_oe_read(volatile uint32_t *base, int bank);
