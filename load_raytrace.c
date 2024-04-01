1daa50115efbe0053bacc7804d4c21a2c1a384cd
James Wilson <jawilso3@synamedia.com>
2024-04-01 18:48:14 +0100

    strip mandel definitions

--- load_raytrace.c
+++ load_raytrace.c
@@ -8,79 +8,6 @@
 #include <stdbool.h>
 #include "lkio.h"
 
-#define HIR  2.5
-#define LOR  3.0e-14
-#define f(x) sqrt(log(x)-log(LOR)+1.0)
-
-#define ASPECT_R  0.75
-#define MIN_SPAN  2.5
-#define CENTER_R -0.75
-#define CENTER_I  0.0
-static int hicnt = 1024;
-static int locnt = 150;
-
-static int calc_iter(double r) {
-    if (r <= LOR) return(hicnt);
-    return((int)((hicnt-locnt)/(f(LOR)-f(HIR))*(f(r)-f(HIR))+locnt+0.5));
-}
-
-static void send_PRBCOM (double center_r, double center_i, double rng, int max_iter) {
-    int len;
-    double xrange,yrange;
-    // This struct shared with transputer code (raytrace.c) so type sizing & ordering is important
-    struct{
-        int32_t com;
-        int32_t width;
-        int32_t height;
-        int32_t maxcnt;
-        double lo_r;
-        double lo_i;
-        double gapx;
-        double gapy;
-    } prob_st;
-    
-    int FLAGS_width = 640;
-    int FLAGS_height = 480;
-
-    int esw,esh;
-    double prop_fac,scale_fac;
-
-    prop_fac = (ASPECT_R/((double)FLAGS_height/(double)FLAGS_width));
-    esw = FLAGS_width;
-    esh = FLAGS_height*prop_fac;
-
-    if (esh <= esw) scale_fac = rng/(esh-1);
-    else scale_fac = rng/(esw-1);
-
-    xrange = scale_fac*(esw-1);
-    yrange = scale_fac*(esh-1);
-    prob_st.com = PRBCOM;
-    prob_st.width = FLAGS_width;
-    prob_st.height = FLAGS_height;
-    prob_st.maxcnt = calc_iter(xrange);
-    prob_st.lo_r = center_r - (xrange/2.0);
-    prob_st.lo_i = center_i - (yrange/2.0);
-    prob_st.gapx = xrange / (FLAGS_width-1);
-    prob_st.gapy = yrange / (FLAGS_height-1);
-#ifdef DEBUG
-    printf ("PRBCOM struct:\n\tcom:%d\n\twidth:%d\n\theight:%d\n\tmaxcnt:%d\n\tlo_r:%lf\n\tlo_i:%lf\n\tgapx:%lf\n\tgapy:%lf\n",
-             prob_st.com, prob_st.width, prob_st.height, prob_st.maxcnt, prob_st.lo_r, prob_st.lo_i, prob_st.gapx, prob_st.gapy);
-    memdump ((uint8_t *)&prob_st,sizeof(prob_st));
-#endif
-    assert (sizeof(prob_st) == 48);
-    if (word_out(sizeof(prob_st)) != 0) {
-        printf(" -- timeout sending prob_st size\n");
-        exit(1);
-    }
-    if (chan_out((char *)&prob_st,sizeof(prob_st)) != 0) {
-        printf(" -- timeout sending prob_st\n");
-        exit(1);
-    }
-#ifdef DEBUG
-    printf ("PRBCOM sent\n");
-#endif    
-}
-
 static void send_object(object *o) {
     if (word_out(c_object) != 0) {
         printf(" -- timeout sending object type\n");
@@ -399,35 +326,27 @@ void do_raytrace(void) {
         }
     }
     pumpWorldModels (selection, patchEdge);
+    if (word_out(c_start) != 0) {
+        printf(" -- timeout sending start\n");
+        exit(1);
+    }
+    printf ("The inmos ray tracer is GO\n");
 
-
-    //RENDER
-    double center_r = CENTER_R;
-    double center_i = CENTER_I;
-    double rng = 3.335073;
-    int max_iter = 145;
-
+    // wait for finish
 	while (1)
 	{
         int len;
-        send_PRBCOM (center_r, center_i, rng, max_iter);
-        // wait for finish
-        while (1) {
-            if (word_in(&len) == 0) {      //len in bytes
-                int32_t buf[RSLCOM_BUFSIZE];
-                if (chan_in ((char *)buf,len) == 0) {
+        if (word_in(&len) == 0) {      //len in bytes
+            int32_t buf[1024];
+            if (chan_in ((char *)buf,len) == 0) {
 #ifdef DEBUG
-                    printf ("len=0x%X, buf = 0x%X 0x%X 0x%X 0x%X...0x%X\n",len,buf[0],buf[1],buf[2],buf[3],buf[(len/4)-1]);
+                printf ("len=0x%X, buf = 0x%X 0x%X 0x%X 0x%X...0x%X\n",len,buf[0],buf[1],buf[2],buf[3],buf[(len/4)-1]);
 #endif
-                    if (buf[0] == FLHCOM) {
-                        printf ("Finished\n");
-                        break;
-                    } 
+                if (buf[0] == c_stop) {
+                    printf ("Finished\n");
+                    break;
                 }
             }
         }
-        // zoom
-        //rng -= 0.5;
-	}
-
+    }
 }
--- rcommon.h
+++ rcommon.h
@@ -1,12 +1,6 @@
 
 /* Definitions shared between the Transputer and host code */
 
-#define JOBCOM 0L
-#define PRBCOM 1L
-#define DATCOM 2L
-#define RSLCOM 3L
-#define FLHCOM 4L
-
 /*-------------------------- COMMANDS PROTOCOL -----------------------------*/
 
 #define c_invalid  -1
@@ -20,6 +14,7 @@
 #define c_map       7 
 #define c_mapAck    8   /* count */
 #define c_message2  9   /* message; p1; p2; size; [ words ] */
+#define c_start     10
 
 #define m_adaptive   0
 #define m_stochastic 1
@@ -187,46 +182,3 @@ typedef struct {
     float screenY[3];
     float pinhole[3];
 } rundata;
-
-#define MAXPIX  32 /* keep it a multiple of 4 please! TODO why can't this be <32? */
-#define MAXPIX_WORDS MAXPIX/4
-
-/* DATCOM {         [buf index]
-    long com;       0 =DATCOM
-    long fxp;       1
-    long maxcnt;    2
-    double lo_r;    3
-    double lo_i;    5
-    double gapx;    7
-    double gapy;    9
-} */
-#define DATCOMSIZE 11
-
-/* JOBCOM {         [buf index]
-    long com;       0 =JOBCOM
-    long x;         1
-    long y;         2
-    long width;     3
-} */
-#define JOBCOMSIZE 4
-
-/* PRBCOM {         [buf index]
-    long com;       0 =PRBCOM
-    long width;     1
-    long height;    2
-    long maxcnt;    3
-    double lo_r;    4
-    double lo_i;    6
-    double gapx;    8
-    double gapy;    10
-} */
-#define PRBSIZE 12 
-
-/* RSLCOM
-0 len
-1 x
-2 y
-3 pixels[len-3]
-*/
-#define RSLCOM_BUFSIZE 3+MAXPIX_WORDS  /* 32 bit words */
-
